daae67a62787944a0df43ffd0c30c488cedc8e00
viziotvhelpcorner <viziotvguide@gmail.com>
2026-03-02 12:00:04 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Quicken Download Guide'
+author = 'Support Guide'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_title = "Quicken Already Purchased | Quicken Download Guide"
+html_show_sourcelink = False
